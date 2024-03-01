ae14772053c6be55ccbc57d17b7e456e95a70f99
Arthur Gabriel <123274385+arthurgmv@users.noreply.github.com>
2024-03-01 07:03:05 -0300

    Add files via upload

---
+++ fizz_buzz.py
@@ -0,0 +1,13 @@
+i = 50
+for number in range(1, i + 1):
+    if number % 3 == 0 and number % 5 == 0:
+        print("fizzbuzz")
+    elif number % 3 == 0:
+        print("fizz")
+    elif number % 5 == 0:
+        print("buzz")
+    else:
+        print(number)
+
+print(i)
+
